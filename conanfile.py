b0520359c90d407259dacdd267778bbc3f59dd75
Loïc Dauphin <astralien3000@yahoo.fr>
2016-06-17 11:30:01 +0200

    stable -> dev in conanfile

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ class AversivePlusPlusModuleConan(ConanFile):
     name = "ik"
     version = "0.1"
     exports = "*.hpp"
-    requires = "cas/0.1@AversivePlusPlus/stable"
+    requires = "cas/0.1@AversivePlusPlus/dev"
 
     def package(self):
         self.copy("*.hpp", src="include", dst="include")
